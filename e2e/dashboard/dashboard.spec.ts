7194946cf39567a2119919be64ad94644c6b53b2
Roberto Oliveira <roberto.oliveira@toughtworks.com>
2024-12-02 11:44:23 -0300

    chore: fix tests e2e

--- e2e/dashboard/dashboard.spec.ts
+++ e2e/dashboard/dashboard.spec.ts
@@ -15,3 +15,32 @@ test("navigate to new user page", async ({ page }) => {
 
   await expect(page).toHaveURL("/#/new-user");
 });
+
+test("should create a new admission", async ({ page }) => {
+  await page.goto("/");
+
+  const newUserButton = page.locator("button", { hasText: "Nova Admissão" });
+  await newUserButton.click();
+
+  await expect(page).toHaveURL("/#/new-user");
+
+  await page.fill('input[placeholder="Digite o nome"]', "João da Silva");
+  await page.fill(
+    'input[placeholder="Digite um e-mail válido"]',
+    "joao.silva@example.com"
+  );
+  await page.fill('input[placeholder="Digite um CPF válido"]', "09680316416");
+  await page.fill('input[type="date"]', "2024-12-15");
+
+  const submitButton = page.locator("button", { hasText: "Cadastrar" });
+  await submitButton.click();
+
+  await expect(
+    page.locator('.Toastify:has-text("Usuário cadastrado com sucesso!")')
+  ).toBeTruthy();
+
+  await expect(page).toHaveURL("/#/dashboard");
+
+  const newAdmission = page.locator("text=João da Silva");
+  await expect(newAdmission).toBeTruthy();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Router from "~/router";
 import { Header } from "./components/Header";
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer data-testid="toast-container" />
       <Header>
         <h1>Caju Front Teste</h1>
       </Header>
